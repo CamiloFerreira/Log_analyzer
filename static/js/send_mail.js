8a983f03829763f9c96983da6cb9d745c7b8339f
Camilo Ferreira <camilo.ferreira@cybertrust.cl>
2022-09-08 12:04:12 -0300

    Modificaciones en apartado de envio de correo

--- static/js/send_mail.js
+++ static/js/send_mail.js
@@ -1,10 +1,29 @@
 
-function sendMail(){
 
 
-    to = $("#to").val()
-    subject = $("#asunto").val()
-    data = {'subject':subject,'from':"Administrador <contacto@mailercyber2.com","to":to,"content":"asdasdasdasdsddasdasdasd"}
+$(document).ready(function(){
+    var myEditor ; 
+    ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .then( editor =>{
+            console.log("Esta iniciado")
+            myEditor = editor
+        })
+        .catch( error => {
+            console.error( error );
+        } );
+        
+
+
+$("#submit").click(function(){
+  
+    to = $("#to").val();
+    subject = $("#asunto").val();
+    text = myEditor.getData();
+    from = $("#from").val()
+
+    data = {'subject':subject,'from':from,"to":to,"content":text};
+   
 
     $.ajax({
         url : "/sendMail",
@@ -18,5 +37,20 @@ function sendMail(){
                 console.log(err)
             }
     })
-}
+
+})
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
 
